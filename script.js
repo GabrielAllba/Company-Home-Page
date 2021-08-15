47becf87e2eef07e78e18d58df9a2732f04256aa
Gabriel Allba <rielgasy9@gmail.com>
2021-08-15 07:28:36 +0700

    Add interactivity in navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+let open = document.getElementById('headlines')
+let menu = document.getElementById('menu')
+let close = document.getElementById('close')
+
+open.addEventListener('click', function(){
+    menu.style.display = 'flex'
+    menu.style.height = "100vh"
+})
+close.addEventListener('click', function(){
+    menu.style.height = '0px'
+})
