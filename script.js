081f5b0efdb9a09ece72980c1a869ed972c5561d
Gabriel Allba <rielgasy9@gmail.com>
2021-08-17 17:56:32 +0700

    add interactivity for viewing product details

--- script.js
+++ script.js
@@ -13,3 +13,60 @@ close.addEventListener('click', function(){
 function myfunction(){
     menu.style.height = '0px'
 }
+
+/*red car*/
+let redViewButton = document.getElementById('red-one')
+let displayRed = document.getElementById('red-pop')
+
+redViewButton.addEventListener('click', function(){
+    displayRed.style.width = '100%'
+})
+
+
+/* close button for popup view*/
+let closePopView = document.getElementsByClassName('close-popup')
+let alldisplay = document.getElementsByClassName('details')
+for(let i = 0; i < closePopView.length; i++){
+    closePopView[i].addEventListener('click', function(){
+        for(let x = 0; x < alldisplay.length; x++){
+            alldisplay[x].style.width = '0%'
+        }
+    })
+}
+
+/*grey car*/
+let greyViewButton = document.getElementById('grey-one')
+let displayGrey = document.getElementById('grey-pop')
+
+greyViewButton.addEventListener('click', function(){
+    displayGrey.style.width = '100%'
+})
+/*yellow car*/
+let yellowViewButton = document.getElementById('yellow-one')
+let displayYellow = document.getElementById('yellow-pop')
+
+yellowViewButton.addEventListener('click', function(){
+    displayYellow.style.width = '100%'
+})
+/*black car*/
+let blackViewButton = document.getElementById('black-one')
+let displayBlack = document.getElementById('black-pop')
+
+blackViewButton.addEventListener('click', function(){
+    displayBlack.style.width = '100%'
+})
+/*white car*/
+let whiteViewButton = document.getElementById('white-one')
+let displayWhite = document.getElementById('white-pop')
+
+whiteViewButton.addEventListener('click', function(){
+    displayWhite.style.width = '100%'
+})
+
+/*maroon car*/
+let maroonViewButton = document.getElementById('maroon-one')
+let displayMaroon = document.getElementById('maroon-pop')
+
+maroonViewButton.addEventListener('click', function(){
+    displayMaroon.style.width = '100%'
+})
